f3da95e3fffa732607b8f01f1ead6ae9357cb060
Yulia Iakovleva <mme.yuliaiakovleva@yandex.ru>
2022-03-20 14:25:25 +0300

    fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,23 +1,21 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 
 module.exports.getCards = (req, res) => {
-  Card
-    .find({})
-    .then((cards) => {res.send({ data: cards })})
+  Card.find({})
+    .then((cards) => {
+      res.send({ data: cards });
+    })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        // const ERROR_CODE = 400;
-       return res
+      if (err.name === "ValidationError") {
+        return res
           .status(400)
           .send({
-            message: 'Переданы некорректные данные при создании карточки.',
+            message: "Переданы некорректные данные при создании карточки.",
           });
-      } else {
-        // const ERROR_CODE = 500;
-        return res
-          .status(500)
-          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
+      return res
+        .status(500)
+        .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
     });
 };
 
@@ -25,17 +23,14 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   // console.log(req.user._id);
   const owner = req.user._id;
-  Card
-    .create({ name, link, owner })
+  Card.create({ name, link, owner })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         const ERROR_CODE = 400;
-        res
-          .status(ERROR_CODE)
-          .send({
-            message: 'Переданы некорректные данные при создании карточки.',
-          });
+        res.status(ERROR_CODE).send({
+          message: "Переданы некорректные данные при создании карточки.",
+        });
       } else {
         const ERROR_CODE = 500;
         res
@@ -48,85 +43,92 @@ module.exports.createCard = (req, res) => {
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
   // console.log(cardId);
-  Card
-    .findByIdAndRemove(cardId)
+  Card.findByIdAndRemove(cardId)
     .orFail(() => {
-      const error = new Error('Карточка с указанным _id не найдена.');
+      const error = new Error("Карточка с указанным _id не найдена.");
       error.statusCode = 404;
       throw error;
     })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      res
-      .status(500)
-      .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "Передан невалидный _id карточки." });
+      }
+      if (err.statusCode === 404) {
+        return res.status(404).send({ message: err.message });
+      }
+      return res
+        .status(500)
+        .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
     });
 };
 
 module.exports.likeCard = (req, res) => {
   const { cardId } = req.params;
 
-  Card
-    .findByIdAndUpdate(
-      cardId,
-      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-      { new: true, runValidators: true}
-    )
+  Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true, runValidators: true }
+  )
     .orFail(() => {
-      const error = new Error('Передан несуществующий _id карточки.');
+      const error = new Error("Передан несуществующий _id карточки.");
       error.statusCode = 404;
       throw error;
     })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400;
-        res
-          .status(ERROR_CODE)
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
           .send({
-            message: 'Переданы некорректные данные для постановки лайка.',
+            message: "Переданы некорректные данные для постановки лайка.",
           });
-      } else if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный _id карточки.' });
       }
-      else {
-        const ERROR_CODE = 500;
-        res
-          .status(ERROR_CODE)
-          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "Передан невалидный _id карточки." });
       }
+      if (err.statusCode === 404) {
+        return res.status(404).send({ message: err.message });
+      }
+      return res
+        .status(500)
+        .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
     });
 };
 
 module.exports.dislikeCard = (req, res) => {
-  Card
-    .findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } }, // убрать _id из массива
-      { new: true, runValidators: true}
-    )
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true, runValidators: true }
+  )
     .orFail(() => {
-      const error = new Error('Передан несуществующий _id карточки.');
+      const error = new Error("Передан несуществующий _id карточки.");
       error.statusCode = 404;
       throw error;
     })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400;
-        res
-          .status(ERROR_CODE)
-          .send({
-            message: 'Переданы некорректные данные для снятия лайка.',
-          });
-      } else if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный _id карточки.' });
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Переданы некорректные данные для снятия лайка." });
       }
-      else {
-        const ERROR_CODE = 500;
-        res
-          .status(ERROR_CODE)
-          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "Передан невалидный _id карточки." });
       }
+      if (err.statusCode === 404) {
+        return res.status(404).send({ message: err.message });
+      }
+      return res
+        .status(500)
+        .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
     });
-  }
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,45 @@
 // const res = require('express/lib/response');
-const user = require('../models/user');
-const User = require('../models/user');
-
+const user = require("../models/user");
+const User = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
-  User
-    .find({})
-    .then((users) => res.send({ data: users}))
+  User.find({})
+    .then((users) => res.send({ data: users }))
     .catch((err) => {
-      if (err.name === 'ValidationError' ) {
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({
+            message: "Переданы некорректные данные при создании пользователя.",
+          });
       } else {
-        res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+        res
+          .status(500)
+          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
-    })
-}
+    });
+};
 
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
   // console.log(userId);
-  User
-    .findById(userId)
+  User.findById(userId)
     .orFail(() => {
-      const error = new Error('Пользователь по указанному _id не найден.');
+      const error = new Error("Пользователь по указанному _id не найден.");
       error.statusCode = 404;
       throw error;
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный id пользователя' });
+      if (err.name === "CastError") {
+        res.status(400).send({ message: "Передан невалидный id пользователя" });
       } else if (err.statusCode === 404) {
         res.status(404).send({ message: err.message });
       } else {
         // const ERROR_CODE = 500;
-        res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+        res
+          .status(500)
+          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
     });
 };
@@ -43,84 +48,101 @@ module.exports.getUserById = (req, res) => {
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User
-    .create({ name, about, avatar }, {
-      new: true,
-      runValidators: true
-    })
+  User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         // const ERROR_CODE = 400;
-        console.log(err)
-        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        console.log(err);
+        res
+          .status(400)
+          .send({
+            message: "Переданы некорректные данные при создании пользователя.",
+          });
       } else {
         // const ERROR_CODE = 500;
-        res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+        res
+          .status(500)
+          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
     });
 };
 
 module.exports.editUser = (req, res) => {
   const { name, about } = req.body;
-//  console.log(req.user._id);
-  User
-    .findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      {
-        new: true,
-        runValidators: true,
-      },
-    )
+  //  console.log(req.user._id);
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
     .orFail(() => {
-      const error = new Error('Пользователь по указанному _id не найден.');
+      const error = new Error("Пользователь по указанному _id не найден.");
       error.statusCode = 404;
       throw error;
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({
+            message: "Переданы некорректные данные при обновлении профиля.",
+          });
       }
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан невалидный id пользователя' });
-      } if (err.statusCode === 404) {
+      if (err.name === "CastError") {
+        return res
+          .status(400)
+          .send({ message: "Передан невалидный id пользователя" });
+      }
+      if (err.statusCode === 404) {
         return res.status(404).send({ message: err.message });
       }
-      return res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}`, ...err });
+      return res
+        .status(500)
+        .send({
+          message: `Произошла ошибка: ${err.name} ${err.message}`,
+          ...err,
+        });
     });
 };
 
-
 module.exports.editUser = (req, res) => {
   const { name, about } = req.body;
-//  console.log(req.user._id);
-  User
-    .findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      {
-        new: true, // обработчик then получит на вход обновлённую запись
-        runValidators: true, // данные будут валидированы перед изменением
-      },
-    )
+  //  console.log(req.user._id);
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true, // обработчик then получит на вход обновлённую запись
+      runValidators: true, // данные будут валидированы перед изменением
+    }
+  )
     .orFail(() => {
-      const error = new Error('Пользователь по указанному _id не найден.');
+      const error = new Error("Пользователь по указанному _id не найден.");
       error.statusCode = 404;
       throw error;
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       console.log(err.name);
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
-      } else if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Передан невалидный id пользователя' });
+      if (err.name === "ValidationError") {
+        res
+          .status(400)
+          .send({
+            message: "Переданы некорректные данные при обновлении профиля.",
+          });
+      } else if (err.name === "CastError") {
+        res.status(400).send({ message: "Передан невалидный id пользователя" });
       } else if (err.statusCode === 404) {
         res.status(404).send({ message: err.message });
       } else {
-        res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+        res
+          .status(500)
+          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
     });
 };
@@ -128,32 +150,36 @@ module.exports.editUser = (req, res) => {
 module.exports.editAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User
-    .findByIdAndUpdate(
-      req.user._id,
-      { avatar },
-      {
-        new: true,
-        runValidators: true,
-      },
-    )
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
     .orFail(() => {
-      const error = new Error('Пользователь по указанному _id не найден.');
+      const error = new Error("Пользователь по указанному _id не найден.");
       error.statusCode = 404;
       throw error;
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         // const ERROR_CODE = 400;
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
-      }
-      else if (err.statusCode === 404) {
+        res
+          .status(400)
+          .send({
+            message: "Переданы некорректные данные при обновлении аватара.",
+          });
+      } else if (err.statusCode === 404) {
         res.status(404).send({ message: err.message });
       } else {
         // const ERROR_CODE = 500;
 
-        res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
+        res
+          .status(500)
+          .send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
       }
     });
 };
