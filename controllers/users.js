84cce5c790c18d267f2b22adc1f3fe5cce659662
Yulia Iakovleva <mme.yuliaiakovleva@yandex.ru>
2022-03-20 11:42:24 +0300

    спасите

--- controllers/users.js
+++ controllers/users.js
@@ -21,11 +21,17 @@ module.exports.getUserById = (req, res) => {
   // console.log(userId);
   User
     .findById(userId)
+    .orFail(() => {
+      const error = new Error('Пользователь по указанному _id не найден.');
+      error.statusCode = 404;
+      throw error;
+    })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'DocumentNotFoundError') {
-        // const ERROR_CODE = 404;
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Передан невалидный id пользователя' });
+      } else if (err.statusCode === 404) {
+        res.status(404).send({ message: err.message });
       } else {
         // const ERROR_CODE = 500;
         res.status(500).send({ message: `Произошла ошибка: ${err.name} ${err.message}` });
@@ -40,7 +46,7 @@ module.exports.createUser = (req, res) => {
     .create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidatorError') {
+      if (err.errors.name.name === 'ValidatorError') {
         // const ERROR_CODE = 400;
         res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
       } else {
@@ -70,7 +76,7 @@ module.exports.editUser = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       console.log(err.name);
-      if (err.name === 'ValidatorError') {
+      if (err.errors.about.name === 'ValidatorError') {
         res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
       } else if (err.name === 'CastError') {
         res.status(400).send({ message: 'Передан невалидный id пользователя' });
